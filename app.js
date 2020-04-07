03982e90a633cf822e4ae635c88e1718cf328b68
Janio Isacura <jisacura@baufest.com>
2020-04-06 23:20:10 -0300

    Se agregó lógica de ruteo y filtrado

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const heroes = JSON.parse(fs.readFileSync(__dirname + '/data/heroes.json', 'utf-
 let listaHeroes = [];
 
 // Ruta Raíz / ➝ Home
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   let mensaje = 'Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio.Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos.Recuerda: ¡nunca pares de creer en ti!.'
   res.send(mensaje);
 });
@@ -23,25 +23,43 @@ app.get('/', (req, res)=>{
 // Ruta /heroes ➝ se envía todo el array y Express lo parsea para el browser como JSON :D
 app.get('/heroes', (req, res) => {
   let getHeroes = heroes.forEach(heroe => {
-    listaHeroes += heroe.nombre + '<br>' + heroe.profesion + '<br>' + heroe.pais + '<br>' + heroe.resenia;
+    listaHeroes += `
+    <h1> ${heroe.nombre}</h1>
+    <strong>Profesión:</strong> <p>${heroe.profesion}</p>
+    <strong>País:</strong> <p>${heroe.pais}</p>
+    <strong>Bio:</strong> <p>${heroe.resenia}</p>;`
   });
   res.send(listaHeroes);
 });
 
 // Ruta /heroes/n ➝ se envía el nombre y profesión del héroe solicitado
-app.get('./heroes/:heroe?', (req, res) => {
+app.get('/heroes/:id', (req, res) => {
   // Acá lo primero será encontrar al héroe que corresponda
   //let heroe = __¿ ? __;
-
+  let getHeroes = heroes.find(heroe => heroe.id == req.params.id);
+  if (req.params.id <= heroes.length) {
+    res.send(`<h1>Hola soy ${getHeroes.nombre}</h1> <h3>y mi profesión es ${getHeroes.profesion}</h3>`);
+  } else {
+    res.send('<h1>Este no es héroe que estás buscando</h1>')
+  }
   // Si se encuentra al héroe se envía el nombre y su profesión
   // Si NO se encuentra se envía el mensaje de no encontrado
 });
 
 // Ruta /heroes/n/bio ➝ se envía la bio del héroe solicitado
-app.get('./heroes/:heroe?/:bio?', (req, res) => {
+app.get('/heroes/:id/:bio?', (req, res) => {
   // Acá lo primero será encontrar al héroe que corresponda
   //let heroe = __¿ ? __;
+  let getHeroes = heroes.find(heroe => heroe.id == req.params.id);
 
+  if (req.params.bio == 'bio') {
+    res.send(`
+    <h1>${getHeroes.nombre}:</h1>
+    <strong>Bio: </strong>
+    <p>${getHeroes.resenia}</p>`);
+  } else {
+    res.send('<h1>Lamento que no desees saber más de mi</h1>');
+  }
   // Si NO se encuentra al héroe se envía un mensaje
   // Si se encuentra al héroe:
   // Se pregunta si vino el parámetro Y el valor esperado y se envía la información
@@ -49,7 +67,11 @@ app.get('./heroes/:heroe?/:bio?', (req, res) => {
 });
 
 // Ruta Créditos
-// ¿?
+app.get('/creditos', (req,res)=>{
+  res.send(`
+    <h1>Realizado por:</h1>
+    <strong>Ariadna Naya, Ezequiel Coletta, Alexandra Velasquez y Janio Isacura</strong>`);
+});
 
 // Ruta... ¿Pára qué sirve esto?
 app.get('*', (req, res) => {
